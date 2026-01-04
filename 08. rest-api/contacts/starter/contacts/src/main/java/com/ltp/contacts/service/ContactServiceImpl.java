8cc3323049638387c3208c7b6b465736fc93f196
martdbj <181728462+martdbj@users.noreply.github.com>
2026-01-04 19:08:45 +0100

    add

--- 08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
+++ 08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
@@ -1,10 +1,14 @@
 package com.ltp.contacts.service;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ltp.contacts.exception.ContactNotFoundException;
+import com.ltp.contacts.exception.NoContactException;
+import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.repository.ContactRepository;
 
 @Service
@@ -12,15 +16,36 @@ public class ContactServiceImpl implements ContactService {
 
     @Autowired
     private ContactRepository contactRepository;
-    
 
-
-
-    private int findIndexById(String id) {
+    private int findIndexById(String id) throws NoContactException {
         return IntStream.range(0, contactRepository.getContacts().size())
             .filter(index -> contactRepository.getContacts().get(index).getId().equals(id))
             .findFirst()
-            .orElseThrow();
+            .orElseThrow(() -> new ContactNotFoundException(id));
     }
 
+    @Override
+    public Contact getContactById(String id) throws NoContactException {
+        return contactRepository.getContact(findIndexById(id));
+    }
+
+    @Override
+    public void saveContact(Contact contact) {
+        contactRepository.saveContact(contact);
+    }
+
+    @Override
+    public void updateContact(String id, Contact contact) throws NoContactException {
+        contactRepository.updateContact(findIndexById(id), contact);
+    }
+
+    @Override
+    public void deleteContact(String id) throws NoContactException{
+        contactRepository.deleteContact(findIndexById(id));
+    }
+
+    @Override
+    public List<Contact> getContacts() {
+        return contactRepository.getContacts();
+    }
 }
